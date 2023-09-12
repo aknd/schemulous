9f462f2d16f1618f23ab5523a5c614a162fb2e62
Akihiro Kondo <akihirokondo511@gmail.com>
2023-09-13 08:41:18 +0900

    Add module exports and rollup config for core and extensions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,42 @@
 import typescript from '@rollup/plugin-typescript';
 
+const commonOutputOptions = {
+  sourcemap: false,
+  exports: 'named',
+};
+
+const commonPlugins = [
+  typescript({
+    tsconfig: './tsconfig.esm.json',
+    sourceMap: false,
+  }),
+];
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createConfig = (input, fileDir, name) => ({
+  input,
+  output: [
+    {
+      file: `${fileDir}/index.mjs`,
+      format: 'es',
+      ...commonOutputOptions,
+    },
+    {
+      file: `${fileDir}/index.umd.js`,
+      format: 'umd',
+      name,
+      ...commonOutputOptions,
+    },
+  ],
+  plugins: commonPlugins,
+});
+
 export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/index.mjs',
-        format: 'es',
-        sourcemap: false,
-        exports: 'named',
-      },
-      {
-        file: 'dist/index.umd.js',
-        name: 'Schemulous',
-        format: 'umd',
-        sourcemap: false,
-        exports: 'named',
-      },
-    ],
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.esm.json',
-        sourceMap: false,
-      }),
-    ],
-  },
+  createConfig('src/index.ts', 'dist', 'Schemulous'),
+  createConfig('src/core/index.ts', 'dist/core', 'SchemulousCore'),
+  createConfig(
+    'src/extensions/index.ts',
+    'dist/extensions',
+    'SchemulousExtensions'
+  ),
 ];
